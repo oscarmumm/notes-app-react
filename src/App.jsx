2fb0631e2e3ba1530af5724801e4707bf4674c5c
Oscar Mumm <oscarmumm@gmail.com>
2023-07-17 13:16:30 -0300

    delete note function ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,35 @@ import NotesCollection from "./components/NotesCollection";
 
 function App() {
     const [notes, setNotes] = useState([]);
+    const [isActive, setIsActive] = useState(false);
 
     const addNoteToCollection = (data) => {
-      setNotes([
-        ...notes,
-        data
-      ]);
-    }
+        setNotes([...notes, data]);
+    };
 
     const deleteFromCollection = (data) => {
-      console.log(data)
-      setNotes(
-        notes.filter(el => el.title !== data )
-      )
-    }
+        console.log(data);
+        setNotes(notes.filter((el) => el.title !== data));
+    };
+
+    const showNewNoteInterface = () => {
+        setIsActive((current) => !current);
+        console.log(isActive);
+    };
 
     return (
         <div className="app">
             <Header />
-            <NewNote addNoteToCollection={addNoteToCollection} />
-            <NotesCollection notes={notes} deleteFromCollection={deleteFromCollection} />
+            <button onClick={showNewNoteInterface} className="new-note-btn">
+                Nueva nota
+            </button>
+            <div className={isActive ? 'desplegar new-note' : 'contraer new-note'}>
+                <NewNote addNoteToCollection={addNoteToCollection} />
+            </div>
+            <NotesCollection
+                notes={notes}
+                deleteFromCollection={deleteFromCollection}
+            />
         </div>
     );
 }
